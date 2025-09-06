0da1541eb57dc997bf624aece1b8052bda98e711
skandakumar1992 <skandakumar1992@gmail.com>
2025-09-06 18:45:57 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello from CI/CD Pipeline 🚀"));
+app.listen(3000, () => console.log("App running on port 3000"));
